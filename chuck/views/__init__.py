fb5d7408b29c3c6fa3e08c4f1e6bca16611296fa
Chuck Grady <cgrady321@gmail.com>
2022-03-06 19:47:53 -0500

    added new pages

--- chuck/views/__init__.py
+++ chuck/views/__init__.py
@@ -1,3 +1,4 @@
 from chuck.views.home import get_homepage
-from chuck.views.projects import get_projects
+from chuck.views.projects import get_projects, get_api, get_nn
+from chuck.views.projects import get_mr, get_piano, get_v2p, get_tsp
 from chuck.views.personal import get_personal
--- chuck/views/projects.py
+++ chuck/views/projects.py
@@ -1,7 +1,30 @@
 import flask
 import chuck
 
-
 @chuck.app.route('/projects/', methods=['GET'])
 def get_projects():
 	return flask.render_template("projects.html")
+
+@chuck.app.route('/projects/api/', methods=['GET'])
+def get_api():
+	return flask.render_template("api.html")
+
+@chuck.app.route('/projects/mr/', methods=['GET'])
+def get_mr():
+	return flask.render_template("mapreduce.html")
+
+@chuck.app.route('/projects/nn/', methods=['GET'])
+def get_nn():
+	return flask.render_template("nn.html")
+
+@chuck.app.route('/projects/piano/', methods=['GET'])
+def get_piano():
+	return flask.render_template("piano.html")
+
+@chuck.app.route('/projects/v2p/', methods=['GET'])
+def get_v2p():
+	return flask.render_template("V2P.html")
+
+@chuck.app.route('/projects/tsp/', methods=['GET'])
+def get_tsp():
+	return flask.render_template("tsp.html")
